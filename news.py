e498e1bb4d67588e3eaf60f7e8384a892d569357
Lucas Hild <lucas@hild-stuttgart.de>
2017-07-25 11:49:14 +0200

    Add news.py

---
+++ news.py
@@ -0,0 +1,49 @@
+import config
+import sqlite3
+import logging
+from telegram.ext import Updater
+
+logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO,
+        filename="bot.log")
+logger = logging.getLogger(__name__)
+
+updater = Updater(token=config.bottoken)
+
+
+def main():
+    # Message input
+    print("Your message (finish with '___'):")
+    new_input = ""
+    message = []
+    while new_input != "___":
+        new_input = input()
+        message.append(new_input)
+
+    # Delete ___ from list
+    del message[-1]
+    message = "\n".join(message)
+
+    # Confirmation
+    confirmation = input("Dou you really want to send? (yes/no): ")
+    if confirmation.lower() != "yes":
+        print("Canceled!")
+        quit()
+
+    # Connect to database
+    conn = sqlite3.connect(config.database_location)
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM users")
+    fetched_users = c.fetchall()
+    c.close()
+    conn.close()
+
+    # Send message to every user
+    for user in fetched_users:
+        updater.bot.send_message(chat_id=user[2], text=message)
+
+
+if __name__ == "__main__":
+    main()
